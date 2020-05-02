a2c568bc4f911a86efc1e7d63c356923e4a317e3
Ireaurora <ireaurora@gmail.com>
2020-05-02 18:55:42 +0100

    cirlces showing

--- dice.js
+++ dice.js
@@ -1,15 +1,19 @@
 function rollingDice(){
+    
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
     var status = document.getElementById("status");
+    var space = document.getElementById("die12");
+    space.innerHTML= "";
     var d1 = Math.floor(Math.random() * 6) + 1;
     var d2 = Math.floor(Math.random() * 6) + 1;
-    for(var first = 0; first <= d1; first++){
-        var x = document.getElementById("1." + first);
-            if(x != null){
-                x.style.color="red";
-            }
+    if(space !=null){
+        console.log(space);
+    for(var i = 0; i < d1; i++){
+        console.log(i);
+       space.innerHTML= space.innerHTML+'<div class="circle"></div>';
     }
+}
     var diceTotal = d1 + d2;
     die1.innerHTML = d1;
     die2.innerHTML = d2;
