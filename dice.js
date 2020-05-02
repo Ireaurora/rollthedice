b74b3c0510fe071c40e522244698222faf47d2cb
Ireaurora <ireaurora@gmail.com>
2020-05-02 20:56:36 +0100

    adding one at the time works

--- dice.js
+++ dice.js
@@ -3,20 +3,20 @@ function rollingDice(){
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
     var status = document.getElementById("status");
-    var space = document.getElementById("die12");
-    space.innerHTML= "";
+    die1.innerHTML= "";
+    die2.innerHTML= "";
     var d1 = Math.floor(Math.random() * 6) + 1;
     var d2 = Math.floor(Math.random() * 6) + 1;
-    if(space !=null){
-        console.log(space);
+    if((die1 && die2) !=null){
+    for(var i = 0; i < d2; i++){
+        die1.innerHTML= die1.innerHTML+'<div class="circle"></div>';
+    }
     for(var i = 0; i < d1; i++){
-        console.log(i);
-       space.innerHTML= space.innerHTML+'<div class="circle"></div>';
+        die2.innerHTML= die2.innerHTML+'<div class="circle"></div>';
     }
 }
     var diceTotal = d1 + d2;
-    die1.innerHTML = d1;
-    die2.innerHTML = d2;
+
     status.innerHTML = "You rolled "+diceTotal+".";
     if(d1 == d2){
         status.innerHTML += " DOUBLES! You get a free turn!!";
